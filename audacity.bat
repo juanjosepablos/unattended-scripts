6ad196e4d68e360e1ef528cf1743b7ea634177b0
Juan Jose Pablos <juanjo@apertus.es>
2013-10-26 11:30:07 +0200

    update 2.0.5

---
+++ audacity.bat
@@ -0,0 +1,6 @@
+:: OPTIONAL: Install Audacity
+:: HOME: http://audacity.sourceforge.net/
+:: URL|ALL|http://downloads.sourceforge.net/audacity/audacity-win-2.0.5.exe|packages/audacity-win-2.0.5.exe
+
+%Z%\packages\audacity-win-2.0.5.exe /silent
+
