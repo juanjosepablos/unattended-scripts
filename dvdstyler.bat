e3bff0283929e10ac2b179fb5f8d65ccdc802273
Juan Jose Pablos <juanjo@apertus.es>
2013-11-12 02:51:26 +0100

    add dvdstyler.bat

---
+++ dvdstyler.bat
@@ -0,0 +1,6 @@
+::OPTIONAL: Install DVDStyler
+::HOME: http://www.dvdstyler.org/
+http://sourceforge.net/projects/dvdstyler/files/dvdstyler/2.5.2/DVDStyler-2.5.2-win32.exe
+::URL|ALL|http://sourceforge.net/projects/dvdstyler/files/dvdstyler/2.5.2/DVDStyler-2.5.2-win32.exe|packages/DVDStyler-2.5.2-win32.exe
+
+@echo off
