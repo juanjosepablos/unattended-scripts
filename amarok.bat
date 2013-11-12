2319cd5814710e94366a10ae32a32fc536c40964
Juan Jose Pablos <juanjo@apertus.es>
2013-11-12 02:45:33 +0100

    add amarok

---
+++ amarok.bat
@@ -0,0 +1,6 @@
+:: OPTIONAL: Amarok
+:: HOME: http://amarok.kde.org/
+:: URL|ALL|http://download.kde.org/download.php?url=stable/amarok/2.5.0/win32/amarok-setup-2.5.0-1.exe|packages/amarok-setup-2.5.0-1.exe
+
+@echo off
+todo.pl "%Z%\packages\amarok-setup-2.5.0-1.exe /S"
