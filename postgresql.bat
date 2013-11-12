fad08032ede579c40d3febed182004b30dba72b9
Juan Jose Pablos <juanjo@apertus.es>
2013-11-12 02:48:06 +0100

    add postgresql.bat

---
+++ postgresql.bat
@@ -0,0 +1,7 @@
+:: OPTIONAL:  Postgresql
+:: HOME: http://www.enterprisedb.com/products-services-training/pgdownload#windows
+:: URL|All|http://get.enterprisedb.com/postgresql/postgresql-9.2.4-1-windows.exe|packages/postgresql/postgresql-9.2.4-1-windows.exe
+
+@Echo off
+
+todo.pl "%Z%\packages\postgresql\postgresql-9.2.4-1-windows.exe --mode=unattended"
