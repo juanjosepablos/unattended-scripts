167f2dc5bfcb76cc0b933533e0a96212f9fec4de
Juan Jose Pablos <juanjo@apertus.es>
2013-11-12 02:51:15 +0100

    add google-earth.bat

---
+++ google-earth.bat
@@ -0,0 +1,7 @@
+:: OPTIONAL: Install Google Earth
+:: HOME: <http://earth.google.com/>
+:: URL|ALL|http://dl.google.com/earth/client/GE5/release_5_1_0/googleearth-Win-5.1.3506.3999.exe|packages/google-earth/google-earth-5.1.3506.3999.exe
+:: URL|ALL|http://dl.google.com/earth/client/advanced/current/GoogleEarthWin.exe|packages/google-earth/google-earth-6.1.0.5001.exe
+@echo off
+
+todo.pl "%Z%\packages\google-earth\google-earth-5.1.3506.3999.exe /S /v/qn"
