0eccb93fa9564b36792f8da3e83b21979106727d
Juan Jose Pablos <juanjo@apertus.es>
2013-11-12 02:46:49 +0100

    add sweethome

---
+++ sweethome.bat
@@ -0,0 +1,8 @@
+:: OPTIONAL:  Install SweetHome 3D
+:: HOME: http://www.sweethome3d.eu/
+:: URL|All|http://downloads.sourceforge.net/sweethome3d/SweetHome3D-2.6-windows.exe|packages/sweethome3d/sweethome3D-2.6-windows.exe
+@Echo off
+
+
+todo.pl "%Z%\packages\sweethome3d\sweethome3D-2.6-windows.exe /S"
+
