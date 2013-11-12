e5606c7a5138342d9e1e9a4771d705339f19130a
Juan Jose Pablos <juanjo@apertus.es>
2013-11-12 02:48:36 +0100

    add openhwmon.bat

---
+++ openhwmon.bat
@@ -0,0 +1,13 @@
+::OPTIONAL: Install OpenHardware Monitor
+:: HOME: http://openhardwaremonitor.org
+:: URL|ALL|http://openhardwaremonitor.org/files/openhardwaremonitor-v0.5.1-beta.zip|packages/openhardwaremonitor-v0.5.1-beta.zip
+
+:: Create shortcut
+
+::todo.pl "shortcut.pl \"%ProgramFiles%\MameUI32\MameUI32.exe\" special:AllusersDesktop\mame"
+
+:: Unzip all of the files within programfiles
+todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -o\"%ProgramFiles%\" %Z%\packages\openhardwaremonitor-v0.5.1-beta.zip"
+
+:: Install 7zip if it isn't already
+todo.pl 7-zip.bat
