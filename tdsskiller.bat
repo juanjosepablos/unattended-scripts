ed2697cea0009502fc65405adf447550490981f2
Juan Jose Pablos <juanjo@apertus.es>
2013-11-15 19:43:23 +0100

    3.0.0.17

--- tdsskiller.bat
+++ tdsskiller.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL: TDSKILLER
 :: HOME: 
-:: URL|ALL|http://media.kaspersky.com/utilities/VirusUtilities/EN/tdsskiller.exe|packages/antirootkit/tdsskiller-3.0.0.16.exe
+:: URL|ALL|http://media.kaspersky.com/utilities/VirusUtilities/EN/tdsskiller.exe|packages/antirootkit/tdsskiller-3.0.0.17.exe
 
 @echo off
 
-todo.pl "tdsskiller-3.0.0.16.exe -silent -accepteula -l %SYSTEMROOT%\tdsskiller.log"
+todo.pl "tdsskiller-3.0.0.17.exe -silent -accepteula -l %SYSTEMROOT%\tdsskiller.log"
 
