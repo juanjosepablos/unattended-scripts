1a74adad22dccd5c974d5dcb8f3c7bb67e22c80b
Juan Jose Pablos <juanjo@apertus.es>
2013-12-15 18:14:11 +0100

    4.08

--- ccleaner.bat
+++ ccleaner.bat
@@ -1,7 +1,7 @@
 ::OPTIONAL: Install Ccleaner
 ::HOME: http://www.piriform.com/CCLEANER
-::URL|ALL|http://download.piriform.com/ccsetup407.exe|packages/ccsetup407.exe
+::URL|ALL|http://download.piriform.com/ccsetup408.exe|packages/ccsetup408.exe
 
 @echo off
 todo.pl "unlink.pl special:AllUsersDesktop\CCleaner.lnk"
-todo.pl "%Z%\packages\ccsetup407.exe /S"
+todo.pl "%Z%\packages\ccsetup408.exe /S"
