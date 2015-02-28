9d9c86773488e2769c4763a7f9ce3f54c854f4d5
Juan Jose Pablos <juanjo@apertus.es>
2015-02-28 10:31:21 +0100

    qcad prinmer version

---
+++ qcad.bat
@@ -0,0 +1,6 @@
+:: OPTIONAL:  Install Qcad
+:: HOME: http://www.qcad.org/
+:: URL|All|http://www.qcad.org/archives/qcad/qcad-3.8.1-win-installer.exe|packages/qcad/qcad-3.8.1-win-installer.exe
+@Echo off
+:: todo.pl "shortcut.pl \"%ProgramFiles%\"\Scribus\scribus.exe special:AllUsersDesktop\Scribus"
+todo.pl "%Z%\packages\qcad\qcad-3.8.1-win-installer.exe /S"
