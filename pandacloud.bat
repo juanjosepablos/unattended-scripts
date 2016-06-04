ab0ee84635cb3e1edf2db01d69fc5b18c0101b17
Juan Jose Pablos <juanjo@apertus.es>
2016-06-04 23:51:00 +0200

    Pandacloud 2016

--- pandacloud.bat
+++ pandacloud.bat
@@ -1,25 +1,16 @@
 :: OPTIONAL: Panda Cloud
 :: HOME: http://www.cloudantivirus.com/
-:: URL|ALL|http://acs.pandasoftware.com/cloud/CloudAntivirus.exe|packages/pandacloud/CloudAntivirus-2.3.exe
-:: URL|ALL|http://www.rarlab.com/rar/wrar501.exe|packages/pandacloud/unrar-x86.exe
-:: URL|ALL|http://www.rarlab.com/rar/winrar-x64-501.exe|packages/pandacloud/unrar-AMD64.exe
+:: URL|ALL|http://acs.pandasoftware.com/Panda2016/FREEAV/181305/FREEAV.exe|packages/pandacloud/FREEAV-181305.exe
 
 @echo off
 :: FIXME Productlanguage is in spanish
 IF [%PROCESSOR_ARCHITECTURE%]==[x86] SET PANDA_MSI=%TEMP%\PandaCloud\PandaCloudAntivirus_x86.msi
 IF [%PROCESSOR_ARCHITECTURE%]==[AMD64] SET PANDA_MSI=%TEMP%\PandaCloud\PandaCloudAntivirus_x64.msi
 
-
-:: 1.5.2
-::todo.pl "%TEMP%\PandaCloud\PandaCloudAntivirus_x86.msi /qb ANALISISDISABLE=1 INSTALLDIR=\"%PROGRAMFILES%\Panda Security\Panda Cloud Antivirus\"  ProductLanguage=1034 ALLOWINFOPANDA=true REBOOT=r SETUPEXEDIR=%TEMP%\PandaCloud\ CURRENTDIRECTORY=%TEMP%\PandaCloud"
-
 todo.pl "%PANDA_MSI% /qb INSTALLDIR=\"%PROGRAMFILES%\Panda Security\Panda Cloud Antivirus\" ProductLanguage=1034 SETUPEXEDIR=%TEMP%\PandaCloud\ CURRENTDIRECTORY=%TEMP%\PandaCloud"
 
-:: Uncompress the program
-todo.pl "%TEMP%\pandacloud\unrar.exe x %Z%\packages\pandacloud\cloudantivirus-2.3.exe %TEMP%\PandaCloud" 
-
-:: For panda 2.3 we need rar formar 5.0 7-zip does not support it yet.
-todo.pl "\"%ProgramFiles%\7-Zip\7z.exe\" x -y -o%TEMP%\PandaCloud %Z%\packages\pandacloud\unrar-%PROCESSOR_ARCHITECTURE%.exe UnRAR.exe"
+:: Uncompress the exe file
+todo.pl "\"%ProgramFiles%\7-Zip\7z.exe\" x -y -o%TEMP%\PandaCloud %Z%\packages\pandacloud\FREEAV-181305.exe"
 
 :: Delete just in case is a reinstall or old version
 todo.pl "del /Q /S %TEMP%\PandaCloud"
